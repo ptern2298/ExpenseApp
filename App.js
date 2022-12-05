0e5797b33fc34e9a37f906469d0d2cf57e1f187e
Pavlo <p.tern2298@gmail.com>
2022-12-05 16:55:43 -0500

    - Screens commit - App commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./screens/HomeScreen";
+import CreateExpenseScreen from "./screens/CreateExpenseScreen";
+import ExpenseDetailsScreen from "./screens/ExpenseDetailsScreen";
+import CameraScreen from "./screens/CameraScreen";
 
+const Stack = createStackNavigator();
 export default function App() {
+
+  const [expenses, setExpenses] = useState([
+    { id: 0, title: 'McDonalds', price: 9.99 },
+    { id: 1, title: 'Shoppers', price: 300.0 },
+    { id: 2, title: 'Stapes', price: 250.0 }
+  ])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home'>
+          <HomeScreen/>
+        </Stack.Screen>
+        <Stack.Screen name='Camera'>
+          <CameraScreen/>
+        </Stack.Screen>
+        <Stack.Screen name='CreateExpense'>
+          <CreateExpenseScreen/>
+        </Stack.Screen>
+        <Stack.Screen name='ExpenseDetails'>
+          <ExpenseDetailsScreen/>
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,28 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, SafeAreaView, ActivityIndicator } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font'
 export default function HomeScreen() {
+    const[loading, setLoading] = useState(false);
+
+    const startLoading = () =>
+    {
+        setLoading(true);
+        setTimeout(() => { setLoading(false)},1000)
+    }
+
+    useEffect(()=> {startLoading()},[])
     return(
-        <></>
+        <SafeAreaView>
+            <ActivityIndicator animating={loading} size='large' style={}/>
+            <>
+                <Text>All Expenses</Text>
+            </>
+            <>
+                <Text>No Expenses Yet!</Text>
+            </>
+            <Button title="Create Expense" onPress={}/>
+            <StatusBar style="auto"/>
+        </SafeAreaView>
     )
 }
